e24243cc2cc144e75a9095b0bd6ebe906adb7d94
zzcyy <laopaisky@gmail.com>
2016-06-24 17:13:13 +0800

    测试类

--- app/src/main/java/com/creatunion/androidframe/activity/MainActivity.java
+++ app/src/main/java/com/creatunion/androidframe/activity/MainActivity.java
@@ -2,17 +2,13 @@ package com.creatunion.androidframe.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.creatunion.androidframe.BaseApplication;
 import com.creatunion.androidframe.R;
-import com.creatunion.androidframe.bean.EventType;
 import com.path.android.jobqueue.JobManager;
 
 import de.greenrobot.event.EventBus;
-import de.greenrobot.event.Subscribe;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
         EventBus.getDefault().register(this);
-        jobManager = BaseApplication.getInstance().getJobManager();
         
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,11 +31,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     
-    @Subscribe
-    public void onEventMainThread(EventType eventType){
-        Log.d(TAG, "run onEventMainThread : "+Thread.currentThread().getId());
-        tv.setText(eventType.getMsg());
-    }
+
     
     @Override
     protected void onDestroy() {
